7f7624cb2d184ff5492dad608ac7352addc8635d
Janario Oliveira <janario.oliveira@tecsinapse.com.br>
2013-04-03 00:22:56 -0300

    Criado instaler para registrar listener de remoção dos temp files

---
+++ src/br/com/tecsinapse/glimpse_netbeans/Installer.java
@@ -0,0 +1,42 @@
+package br.com.tecsinapse.glimpse_netbeans;
+
+import br.com.tecsinapse.glimpse_netbeans.service.TempGlimpseAction;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.modules.ModuleInstall;
+import org.openide.text.DataEditorSupport;
+import org.openide.util.Exceptions;
+import org.openide.windows.TopComponent;
+
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        TopComponent.Registry registry = TopComponent.getRegistry();
+        registry.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                if (evt.getPropertyName().equals(TopComponent.Registry.PROP_TC_CLOSED)
+                        && evt.getNewValue() instanceof TopComponent) {
+                    TopComponent tp = (TopComponent) evt.getNewValue();
+                    DataEditorSupport data = tp.getLookup().lookup(DataEditorSupport.class);
+                    if (data != null && data.getDataObject() != null
+                            && data.getDataObject().getPrimaryFile() != null) {
+                        FileObject tempDir = TempGlimpseAction.getGlimpseDir();
+                        FileObject primaryFile = data.getDataObject().getPrimaryFile();
+                        if (FileUtil.isParentOf(tempDir, primaryFile)) {
+                            try {
+                                primaryFile.delete();
+                            } catch (IOException ex) {
+                                Exceptions.printStackTrace(ex);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
